0888d02df8e251dd14024e9b7541fa3dc4524d51
Tobit Misoi <misoitobit@zetech.ac.ke>
2021-03-21 12:11:06 +0300

    |footer|

--- src/components/layout/footer/footer.js
+++ src/components/layout/footer/footer.js
@@ -1,10 +1,86 @@
-import React from 'react'
+import React from 'react';
+import { FaEnvelope, FaFacebook, FaInstagram, FaPhone, FaTwitter, FaYoutube } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 
 function Footer() {
     return (
-        <footer>
-            Footer section
-        </footer>  
+        <footer class="footer-distributed row ">
+            <div class="footer-left aligner">
+                <h3>
+                    lancer
+                    <span>Technologies</span>
+                </h3>
+                <div class="footer-links aligner">
+                    <Link to="/">
+                        Home
+                    </Link>
+                    <Link to="/blog">
+                        Blog
+                    </Link>
+                    <Link to="/services">
+                        Services we offer
+                    </Link>
+                    <Link to="/projects">
+                        Our projects
+                    </Link>
+                    <Link to="/about">
+                        About us
+                    </Link>
+                    <Link to="/contacts">
+                        Contacts
+                    </Link>
+                </div>
+            </div>
+            <div class="footer-center aligner">
+                <div>
+                    <address>
+                        21 Revolution Street<br />
+                        Nairobi, Kenya
+                    </address>
+                </div>
+                <div>
+
+                    <span>
+                        <FaPhone />
+                    </span>
+                    <p>
+                        +254 700000000
+                    </p>
+                </div>
+                <div>
+
+                    <span>
+                        <FaEnvelope />
+                    </span>
+                    <p>
+                        <a href="mailto:support@company.com">
+                            contact@lancertechnologies.com
+                        </a>
+                    </p>
+                </div>
+            </div>
+            <div class="footer-right aligner">
+                <p class="footer-company-about"></p>
+                <span>About the company</span>
+                <p>#####</p>
+                <div class="footer-icons">
+                    <a href="www.facebook.com">
+                        <FaFacebook />
+                    </a>
+                    <a href="www.twitter.com">
+                        <FaTwitter />
+                    </a>
+                    <a href="www.instagram.com">
+                        <FaInstagram />
+                    </a>
+                    <a href="www.youtube.com">
+                        <FaYoutube />
+                    </a>
+                </div>
+                <p class="footer-company-name">&copy;2021 Lancer Technologies</p>
+
+            </div>
+        </footer>
     )
 }
 
--- src/components/layout/header/header.jsx
+++ src/components/layout/header/header.jsx
@@ -5,7 +5,43 @@ function Header(props) {
     return (
         <>
             <header>
-                Header section
+                <main>
+                    <div>
+                        <nav class="navbar navbar-expand-lg navbar-dark bg-dark rounded text-light" aria-label="Eleventh navbar example">
+                            <div class="container-fluid">
+                                <a className="navbar-brand" href="/">Lancer Technologies </a>
+                                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample09" aria-controls="navbarsExample09" aria-expanded="false" aria-label="Toggle navigation">
+                                    <span class="navbar-toggler-icon"></span>
+                                </button>
+
+                                <div class="collapse navbar-collapse" id="navbarsExample09">
+                                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                                        <li className="nav-item">
+                                            <a className="nav-link active" aria-current="page" href="/">Home</a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a className="nav-link active" href="/">Projects</a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a className="nav-link active" href="/" tabindex="-1">Blog</a>
+                                        </li>
+                                        <li class="nav-item dropdown">
+                                            <a className="nav-link dropdown-toggle" href="/" id="dropdown09" data-bs-toggle="dropdown" aria-expanded="false">Dropdown</a>
+                                            <ul class="dropdown-menu " aria-labelledby="dropdown09">
+                                                <li><a class="dropdown-item" href="/">Services</a></li>
+                                                <li><a class="dropdown-item" href="/">Contacts</a></li>
+                                                <li><a class="dropdown-item" href="/">About Us</a></li>
+                                            </ul>
+                                        </li>
+                                    </ul>
+                                    <form action="#">
+                                        <input class="form-control" type="text" placeholder="Search" aria-label="Search" />
+                                    </form>
+                                </div>
+                            </div>
+                        </nav>
+                    </div>
+                </main>
             </header>
         </>
     )
