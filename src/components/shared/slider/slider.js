045498e9a4549d54d4a5fd78e7e430dac628e802
Tobit Misoi <misoitobit@zetech.ac.ke>
2021-04-30 11:21:23 +0300

    prop-types

--- src/components/shared/slider/slider.js
+++ src/components/shared/slider/slider.js
@@ -1,61 +1,94 @@
-import React, { useRef, useState, useEffect } from 'react';
-
+import React, { useRef, useState, useEffect } from 'react'
+import PropTypes from 'prop-types'
 
 const Slider = ({ children, showNav, startFrom }) => {
-    const slider = useRef();
-    const [activeSlide, setActiveSlide] = useState(startFrom ? startFrom : 0);
+  const slider = useRef()
+  const [activeSlide, setActiveSlide] = useState(startFrom ? startFrom : 0)
 
-    useEffect(() => {
-        if (startFrom) {
-            slider.current.style.transform = `translateX(${startFrom * -100}%)`;
-        }
-    }, [startFrom]);
-
-    const showPrevSlide = () => {
-        if (activeSlide > 0) {
-            slider.current.style.transform = `translateX(${(activeSlide - 1) * -100}%)`;
-            setActiveSlide(prev => prev - 1);
-        }
+  useEffect(() => {
+    if (startFrom) {
+      slider.current.style.transform = `translateX(${startFrom * -100}%)`
     }
+  }, [startFrom])
 
-    const showNextSlide = () => {
-        if (activeSlide < children.length - 1) {
-            slider.current.style.transform = `translateX(${(activeSlide + 1) * -100}%)`;
-            setActiveSlide(prev => prev + 1);
-        }
+  const showPrevSlide = () => {
+    if (activeSlide > 0) {
+      slider.current.style.transform = `translateX(${
+        (activeSlide - 1) * -100
+      }%)`
+      setActiveSlide((prev) => prev - 1)
     }
+  }
 
-    const updateNavElements = (index) => {
-        setActiveSlide(index);
-        slider.current.style.transform = `translateX(${index * -100}%)`;
+  const showNextSlide = () => {
+    if (activeSlide < children.length - 1) {
+      slider.current.style.transform = `translateX(${
+        (activeSlide + 1) * -100
+      }%)`
+      setActiveSlide((prev) => prev + 1)
     }
+  }
 
-    return (
-        <div className="slider-wrapper">
-            <div className="slider" ref={slider}>
-                {children}
-            </div>
+  const updateNavElements = (index) => {
+    setActiveSlide(index)
+    slider.current.style.transform = `translateX(${index * -100}%)`
+  }
 
-            <div className="slider-wrapper__buttons">
-                <button onClick={showPrevSlide} disabled={activeSlide === 0}>Prev</button>
-                <button onClick={showNextSlide} disabled={activeSlide === children.length - 1}>Next</button>
-            </div>
+  return (
+    <div className='slider-wrapper'>
+      <div className='slider' ref={slider}>
+        {children}
+      </div>
 
-            {showNav && <div className="slider-wrapper__nav">
-                {children.map((s, i) => <span key={i} className={activeSlide === i ? 'active' : ''} onClick={() => updateNavElements(i)}></span>)}
-            </div>}
+      <div className='slider-wrapper__buttons'>
+        <button onClick={showPrevSlide} disabled={activeSlide === 0}>
+          Prev
+        </button>
+        <button
+          onClick={showNextSlide}
+          disabled={activeSlide === children.length - 1}
+        >
+          Next
+        </button>
+      </div>
+
+      {showNav && (
+        <div className='slider-wrapper__nav'>
+          {children.map((s, i) => (
+            <span
+              key={i}
+              className={activeSlide === i ? 'active' : ''}
+              onClick={() => updateNavElements(i)}
+            ></span>
+          ))}
         </div>
-    );
+      )}
+    </div>
+  )
 }
 
 const Slide = ({ bg, children }) => {
-    return (
-        <div className="slider__slide">
-            <div className="slider__slide_content" style={bg ? { backgroundImage: `url(${bg})` } : null}>
-                {children}
-            </div>
-        </div>
-    );
+  return (
+    <div className='slider__slide'>
+      <div
+        className='slider__slide_content'
+        style={bg ? { backgroundImage: `url(${bg})` } : null}
+      >
+        {children}
+      </div>
+    </div>
+  )
 }
 
-export { Slide, Slider };
+Slide.propTypes = {
+  bg: PropTypes.any,
+  children: PropTypes.node
+}
+
+Slider.propTypes = {
+  children: PropTypes.node,
+  showNav: PropTypes.func,
+  startFrom: PropTypes.func
+}
+
+export { Slide, Slider }
