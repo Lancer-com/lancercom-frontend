da6e5cf7aed69fbc06d0b494ecbe886898be3712
Tobit Misoi <misoitobit@zetech.ac.ke>
2021-04-30 11:49:24 +0300

    scss path upd

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import './assets/css/main.scss';
+import './assets/styles/scss/main.scss';
 
 ReactDOM.render(
   <React.StrictMode>
